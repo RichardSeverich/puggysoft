8bc39027ff987baacf4ec3e7948750e7d05b1933
Fernando Ojeda <fernandoojeda702@gmail.com>
2023-06-19 18:43:25 -0400

    Change the salary records in the sql file  and add the table name for all fields in the table too

--- backend/backend-001/sql/db-patches-insert-data-test-11-planilla.sql
+++ backend/backend-001/sql/db-patches-insert-data-test-11-planilla.sql
@@ -2,22 +2,22 @@
 INSERT INTO sueldos_detalle_contratacion (empleado, fecha_ingreso, cargo, haber_basico, tenant) VALUES
 ("ADRIANA_HERNANDEZ", "2021-12-01", "Gerente General", "10000", "EMPRESA_1");
 INSERT INTO sueldos_detalle_contratacion (empleado, fecha_ingreso, cargo, haber_basico, tenant) VALUES
-("ADRIANA_HERNANDEZ", "2022-01-01", "Director Operaciones", "10000", "EMPRESA_1");
+("ESTEBAN_BOTERO", "2022-01-01", "Director Operaciones", "11000", "EMPRESA_2");
 INSERT INTO sueldos_detalle_contratacion (empleado, fecha_ingreso, cargo, haber_basico, tenant) VALUES
-("ADRIANA_HERNANDEZ", "2022-02-01", "Director Comercial", "10000", "EMPRESA_1");
+("ALEJANDRO_ABONDANO", "2022-02-01", "Director Comercial", "20000", "EMPRESA_3");
 INSERT INTO sueldos_detalle_contratacion (empleado, fecha_ingreso, cargo, haber_basico, tenant) VALUES
-("ADRIANA_HERNANDEZ", "2022-03-01", "Director de Marketing", "10000", "EMPRESA_1");
+("NATALIA_CONTRERAS", "2022-03-01", "Director de Marketing", "30000", "EMPRESA_1");
 INSERT INTO sueldos_detalle_contratacion (empleado,fecha_ingreso, cargo, haber_basico, tenant) VALUES
-("ADRIANA_HERNANDEZ", "2022-04-01", "Director de Recursos Humanos", "10000", "EMPRESA_1");
+("cgarcia", "2022-04-01", "Director de Recursos Humanos", "10000", "EMPRESA_2");
 INSERT INTO sueldos_detalle_contratacion (empleado, fecha_ingreso, cargo, haber_basico, tenant) VALUES
-("ADRIANA_HERNANDEZ", "2022-05-01", "Director Financiero", "10000", "EMPRESA_1");
+("mrodrigo", "2022-05-01", "Director Financiero", "10000", "EMPRESA_3");
 INSERT INTO sueldos_detalle_contratacion (empleado, fecha_ingreso, cargo, haber_basico, tenant) VALUES
-("ADRIANA_HERNANDEZ", "2022-06-01", "Empleado 1", "10000", "EMPRESA_1");
+("iortega", "2022-06-01", "Empleado 1", "15000", "EMPRESA_1");
 INSERT INTO sueldos_detalle_contratacion (empleado, fecha_ingreso, cargo, haber_basico, tenant) VALUES
-("ADRIANA_HERNANDEZ", "2022-07-01", "Empleado 2", "10000", "EMPRESA_1");
+("esyler", "2022-07-01", "Empleado 2", "19000", "EMPRESA_2");
 INSERT INTO sueldos_detalle_contratacion (empleado, fecha_ingreso, cargo, haber_basico, tenant) VALUES
-("ADRIANA_HERNANDEZ", "2022-08-01", "Empleado 3", "10000", "EMPRESA_1");
+("icruz", "2022-08-01", "Empleado 3", "10000", "EMPRESA_3");
 INSERT INTO sueldos_detalle_contratacion (empleado, fecha_ingreso, cargo, haber_basico, tenant) VALUES
-("ADRIANA_HERNANDEZ", "2022-09-01", "Empleado 4", "10000", "EMPRESA_1");
+("dvera", "2022-09-01", "Empleado 4", "10000", "RESTAURANTE_1");
 INSERT INTO sueldos_detalle_contratacion (empleado, fecha_ingreso, cargo, haber_basico, tenant) VALUES
-("ADRIANA_HERNANDEZ", "2022-10-01", "Empleado 5", "10000", "EMPRESA_1");
+("ANDRES_VILLA", "2022-10-01", "Empleado 5", "10000", "FERRETERIA_1");
--- backend/backend-001/src/main/java/com/puggysoft/tools/salary/SqlSalaryEmployeeFilterBuilderNative.java
+++ backend/backend-001/src/main/java/com/puggysoft/tools/salary/SqlSalaryEmployeeFilterBuilderNative.java
@@ -12,16 +12,17 @@ public final class SqlSalaryEmployeeFilterBuilderNative {
    * @return the salary employee query.
    */
   public static String build(DtoSalaryEmployeeFilter dtoSalaryEmployeeFilter) {
+    //sueldos_detalle_contratacion
     String query = "";
-    query = query + SqlFilterBuilder.getFilterQuery("empleado", dtoSalaryEmployeeFilter.empleadoCriteria, dtoSalaryEmployeeFilter.empleadoOperator);
-    query = query + SqlFilterBuilder.getFilterQuery("fecha_ingreso", dtoSalaryEmployeeFilter.fechaIngresoCriteria, dtoSalaryEmployeeFilter.fechaIngresoOperator);
-    query = query + SqlFilterBuilder.getFilterQuery("cargo ", dtoSalaryEmployeeFilter.cargoCriteria, dtoSalaryEmployeeFilter.cargoOperator);
-    query = query + SqlFilterBuilder.getFilterQuery("haber_basico", dtoSalaryEmployeeFilter.haberBasicoCriteria, dtoSalaryEmployeeFilter.haberBasicoOperator);
-    query = query + SqlFilterBuilder.getFilterQuery("tenant", dtoSalaryEmployeeFilter.tenantCriteria, dtoSalaryEmployeeFilter.tenantOperator);
-    query = query + SqlFilterBuilder.getFilterQuery("created_by", dtoSalaryEmployeeFilter.createdByCriteria, dtoSalaryEmployeeFilter.createdByOperator);
-    query = query + SqlFilterBuilder.getFilterQuery("updated_by", dtoSalaryEmployeeFilter.updatedByCriteria, dtoSalaryEmployeeFilter.updatedByOperator);
-    query = query + SqlFilterBuilder.getFilterQuery("creation_date", dtoSalaryEmployeeFilter.creationDateCriteria, dtoSalaryEmployeeFilter.creationDateOperator);
-    query = query + SqlFilterBuilder.getFilterQuery("update_date",  dtoSalaryEmployeeFilter.updateDateCriteria, dtoSalaryEmployeeFilter.updateDateOperator);
+    query = query + SqlFilterBuilder.getFilterQuery("sueldos_detalle_contratacion.empleado", dtoSalaryEmployeeFilter.empleadoCriteria, dtoSalaryEmployeeFilter.empleadoOperator);
+    query = query + SqlFilterBuilder.getFilterQuery("sueldos_detalle_contratacion.fecha_ingreso", dtoSalaryEmployeeFilter.fechaIngresoCriteria, dtoSalaryEmployeeFilter.fechaIngresoOperator);
+    query = query + SqlFilterBuilder.getFilterQuery("sueldos_detalle_contratacion.cargo ", dtoSalaryEmployeeFilter.cargoCriteria, dtoSalaryEmployeeFilter.cargoOperator);
+    query = query + SqlFilterBuilder.getFilterQuery("sueldos_detalle_contratacion.haber_basico", dtoSalaryEmployeeFilter.haberBasicoCriteria, dtoSalaryEmployeeFilter.haberBasicoOperator);
+    query = query + SqlFilterBuilder.getFilterQuery("sueldos_detalle_contratacion.tenant", dtoSalaryEmployeeFilter.tenantCriteria, dtoSalaryEmployeeFilter.tenantOperator);
+    query = query + SqlFilterBuilder.getFilterQuery("sueldos_detalle_contratacion.created_by", dtoSalaryEmployeeFilter.createdByCriteria, dtoSalaryEmployeeFilter.createdByOperator);
+    query = query + SqlFilterBuilder.getFilterQuery("sueldos_detalle_contratacion.updated_by", dtoSalaryEmployeeFilter.updatedByCriteria, dtoSalaryEmployeeFilter.updatedByOperator);
+    query = query + SqlFilterBuilder.getFilterQuery("sueldos_detalle_contratacion.creation_date", dtoSalaryEmployeeFilter.creationDateCriteria, dtoSalaryEmployeeFilter.creationDateOperator);
+    query = query + SqlFilterBuilder.getFilterQuery("sueldos_detalle_contratacion.update_date",  dtoSalaryEmployeeFilter.updateDateCriteria, dtoSalaryEmployeeFilter.updateDateOperator);
     return query;
   }
 }
