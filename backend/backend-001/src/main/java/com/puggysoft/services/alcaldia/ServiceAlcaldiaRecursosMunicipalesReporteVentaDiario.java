bd59e5bef3187521b5544c0896203df9b362bf74
“RichardSeverich” <richard.severich@gmail.com>
2024-07-02 16:14:59 -0400

    fix timbres report

--- backend/backend-001/src/main/java/com/puggysoft/services/alcaldia/ServiceAlcaldiaRecursosMunicipalesReporteVentaDiario.java
+++ backend/backend-001/src/main/java/com/puggysoft/services/alcaldia/ServiceAlcaldiaRecursosMunicipalesReporteVentaDiario.java
@@ -2,6 +2,8 @@ package com.puggysoft.services.alcaldia;
 
 import com.puggysoft.dtos.alcaldia.DtoAlcaldiaRecursosMunicipalesReporteCriteriaDay;
 import com.puggysoft.dtos.alcaldia.DtoAlcaldiaRecursosMunicipalesVenta;
+import com.puggysoft.entities.alcaldia.EntityAlcaldiaRecursosMunicipalesVentaDetalle;
+import com.puggysoft.repositories.alcaldia.IRepositoryAlcaldiaRecursosMunicipalesVentaDetalle;
 import com.puggysoft.repositories.alcaldia.IRepositoryAlcaldiaRecursosMunicipalesVentasReporte;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,21 +19,39 @@ public class ServiceAlcaldiaRecursosMunicipalesReporteVentaDiario {
   @Autowired
   private IRepositoryAlcaldiaRecursosMunicipalesVentasReporte IRRecusosMunicipalesVentasReporte;
 
+  @Autowired
+  private IRepositoryAlcaldiaRecursosMunicipalesVentaDetalle repositoryVentaDetalles;
+
   /**
    * method for retrieve.
    */
   public ResponseEntity<List<DtoAlcaldiaRecursosMunicipalesVenta>> getReporteVentasDiario(
-      DtoAlcaldiaRecursosMunicipalesReporteCriteriaDay dtoCriteria){
-
+      DtoAlcaldiaRecursosMunicipalesReporteCriteriaDay dtoCriteria) {
     List<DtoAlcaldiaRecursosMunicipalesVenta> listDtoVentasDiarias = IRRecusosMunicipalesVentasReporte.getDailyReport(
         dtoCriteria.nameRecursoMunicipal,
-            dtoCriteria.idRecursoMunipal,
-            dtoCriteria.status.toString(),
-            dtoCriteria.tenant,
-            dtoCriteria.yearMonthDay)
+        dtoCriteria.idRecursoMunipal,
+        dtoCriteria.status.toString(),
+        dtoCriteria.tenant,
+        dtoCriteria.yearMonthDay)
         .stream().map(DtoAlcaldiaRecursosMunicipalesVenta::entityToDto)
         .collect(Collectors.toList());
 
+    for (DtoAlcaldiaRecursosMunicipalesVenta venta : listDtoVentasDiarias) {
+      // Obtenemos todos los detalles de una venta, (Estos detalles pueden incluir otros recursos vendidos)
+      List<EntityAlcaldiaRecursosMunicipalesVentaDetalle> ventaDetalle = repositoryVentaDetalles
+          .getVentasDeUnVenta(new Long(dtoCriteria.idRecursoMunipal),
+              dtoCriteria.status.toString(),
+              dtoCriteria.tenant,
+              venta.getId());
+      // Recorremos todos los detalles y verificamos que solo sumen los que son timbres
+      for (EntityAlcaldiaRecursosMunicipalesVentaDetalle detalle : ventaDetalle) {
+        Double ingresoVenta = 0d;
+        if (detalle.getIdRecursoMunicipal().equals(dtoCriteria.idRecursoMunipal.toString())) {
+          ingresoVenta = ingresoVenta + Double.parseDouble(detalle.getCantidad()) * Double.parseDouble(detalle.getPrecioUnidad());
+        }
+        venta.setVentaPrecioTotal(ingresoVenta.toString());
+      }
+    }
     return ResponseEntity.status(HttpStatus.OK).body(listDtoVentasDiarias);
   }
 }
